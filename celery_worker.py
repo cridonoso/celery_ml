970a917090b53ce0d8a94526380c434ce10a4971
cristobal <cridonoso@inf.udec.cl>
2022-10-20 16:55:49 -0300

    update

--- celery_worker.py
+++ celery_worker.py
@@ -5,10 +5,10 @@ from celery import Celery
 from time import sleep
 from dotenv import load_dotenv
 
-sys.path.append('/home/refactoring')
-sys.path.append('/home/refactoring/presentation')
+sys.path.append('/home/repo')
+sys.path.append('/home/repo/presentation')
 
-from presentation.scripts.run_0 import train
+from presentation.scripts.run_0 import train, classify
 
 
 load_dotenv(".env")
@@ -20,11 +20,14 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 @celery.task(name='create_task')
 def create_task():
-    print(os.getcwd())
-    os.chdir("refactoring")
+    os.chdir("repo")
     config_file = 'presentation/pipeline/config/template.toml'
+
+    root_folder = os.environ['CONFIG_FOLDER']
+    print(root_folder)
 
     train(config_file,
           history_path='results/history.csv',
           step='pretraining',
-          pipeline_id=None)
+          pipeline_id=None,
+          gpu=0)
